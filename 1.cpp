2f7cd27ad730c6088a6ba33582f1339643d34301
catosaurusrex2003 <mohdmehdi2003@gmail.com>
2023-08-22 21:53:34 +0530

    start

--- 1.cpp
+++ 1.cpp
@@ -4,34 +4,82 @@ using namespace std;
 #define F first
 #define S second
 #define pb push_back
-#define cy cout << "YES"<<endl;
-#define cn cout << "NO "<<endl;
+#define cy cout << "YES" << endl;
+#define cn cout << "NO " << endl;
 #define mod 1000000007
 #define all(x) x.begin(), x.end()
 #define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
-void leftrotate(vector<int>& s, int d){reverse(s.begin(), s.begin()+d);reverse(s.begin()+d, s.end());reverse(s.begin(), s.end());}
-void rightrotate(vector<int>& s, int d) {leftrotate(s, s.size()-d);}
+void leftrotate(vector<int> &s, int d)
+{
+    reverse(s.begin(), s.begin() + d);
+    reverse(s.begin() + d, s.end());
+    reverse(s.begin(), s.end());
+}
+void rightrotate(vector<int> &s, int d) { leftrotate(s, s.size() - d); }
 //------> for (auto x : v) can be used on map vector set and cannot be used to take input" ";
 template <typename Arg1>
-void __f (const char* name, Arg1&& arg1) {cout << name << ':' << arg1 << endl;}
+void __f(const char *name, Arg1 &&arg1) { cout << name << ':' << arg1 << endl; }
 template <typename Arg1, typename... Args>
-void __f (const char* names, Arg1&& arg1, Args&&... args)
+void __f(const char *names, Arg1 &&arg1, Args &&...args)
 {
-const char* comma = strchr (names + 1, ',');
-cout.write (names, comma - names) << ':' << arg1 << '|'; __f (comma + 1, args...);   // MIGHT NEED TO CHANGE THIS
+    const char *comma = strchr(names + 1, ',');
+    cout.write(names, comma - names) << ':' << arg1 << '|';
+    __f(comma + 1, args...); // MIGHT NEED TO CHANGE THIS
 }
 void solve();
 int32_t main()
 {
-ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-int t = 1;
-// cin >> t;
-while (t--) solve();
-return 0;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
 }
-void printvect(vector <int> v ) { for(int i = 0 ; i<v.size() ; i++ ){ cout << v[i] << " "; }cout<<endl; }
-template<typename K, typename V>
-void print_map(std::unordered_map<K, V> const &m){for (auto const &pair: m) {std::cout << "{" << pair.first << ": " << pair.second << "}\n";}}
+void printvect(vector<int> v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+template <typename K, typename V>
+void print_map(std::unordered_map<K, V> const &m)
+{
+    for (auto const &pair : m)
+    {
+        std::cout << "{" << pair.first << ": " << pair.second << "}\n";
+    }
+}
 void solve()
 {
+    int n, cache;
+    cin >> n;
+    vector<int> a, b, diff;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> cache;
+        a.pb(cache);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> cache;
+        b.pb(cache);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        diff.pb(a[i] - b[i]);
+    }
+    int maxElem = *max_element(diff.begin(),diff.end());
+    int maxCount = count(diff.begin(),diff.end(),maxElem);
+    cout<<maxCount<<endl;
+    for(int i = 0 ; i < n ; i ++){
+        if(diff[i] == maxElem){
+            cout<<i+1<<" ";
+        }
+    }
+    cout<<endl;
 }
--- 2.cpp
+++ 2.cpp
@@ -1,37 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long int
-#define F first
-#define S second
-#define pb push_back
-#define cy cout << "YES"<<endl;
-#define cn cout << "NO "<<endl;
-#define mod 1000000007
-#define all(x) x.begin(), x.end()
-#define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
-void leftrotate(vector<int>& s, int d){reverse(s.begin(), s.begin()+d);reverse(s.begin()+d, s.end());reverse(s.begin(), s.end());}
-void rightrotate(vector<int>& s, int d) {leftrotate(s, s.size()-d);}
-//------> for (auto x : v) can be used on map vector set and cannot be used to take input" ";
-template <typename Arg1>
-void __f (const char* name, Arg1&& arg1) {cout << name << ':' << arg1 << endl;}
-template <typename Arg1, typename... Args>
-void __f (const char* names, Arg1&& arg1, Args&&... args)
-{
-const char* comma = strchr (names + 1, ',');
-cout.write (names, comma - names) << ':' << arg1 << '|'; __f (comma + 1, args...);   // MIGHT NEED TO CHANGE THIS
+int dp[50][20011];
+bool solve(int i, int target, int arr[]){
+    if (target == 0){
+        return true;
+    }
+    if (i < 0){
+        return false;
+    }
+    if (dp[i][target] != -1){
+        return dp[i][target];
+    }
+    bool ans = solve(i - 1, target, arr);
+    if (arr[i] <= target){
+        ans = (ans || solve(i - 1, target - arr[i], arr));
+    }
+    return dp[i][target] = ans;
 }
-void solve();
-int32_t main()
+int main()
 {
-ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-int t = 1;
-// cin >> t;
-while (t--) solve();
-return 0;
-}
-void printvect(vector <int> v ) { for(int i = 0 ; i<v.size() ; i++ ){ cout << v[i] << " "; }cout<<endl; }
-template<typename K, typename V>
-void print_map(std::unordered_map<K, V> const &m){for (auto const &pair: m) {std::cout << "{" << pair.first << ": " << pair.second << "}\n";}}
-void solve()
-{
+    int t;
+    cin >> t;
+    while (t--){
+        int n, m;
+        cin >> n >> m;
+        int arr[n];
+        for (int i = 0; i < n; ++i){
+            cin >> arr[i];
+        }
+        for (int i = 0; i < n + 5; ++i){
+            for (int j = 0; j < m + 5; ++j){
+                dp[i][j] = -1;
+            }
+        }
+        if (solve(n - 1, m, arr)){
+            cout << "Yes\n";
+        }
+        else{
+            cout << "No\n";
+        }
+    }
+    return 0;
 }
